45c91dc7f882c79528974dec73a7fe5b34f21b41
evouga <evouga@gmail.com>
2021-01-09 03:19:52 -0600

    Combine duplicate soup DOFs

--- src/Integration.cpp
+++ src/Integration.cpp
@@ -6,8 +6,59 @@
 #include "FrameField.h"
 #include <set>
 #include <Eigen/Dense>
+#include <vector>
 
 namespace CubeCover {
+
+    class UnionFind {
+    public:
+        UnionFind(int nelements)
+        {
+            parent.resize(nelements);
+            sign.resize(nelements);
+            for (int i = 0; i < nelements; i++)
+            {
+                parent[i] = i;
+                sign[i] = 1;
+            }
+        }
+
+        void find(int idx, int &label, int &labelsign)
+        {
+            int root = idx;
+            int signtoroot = 1;
+            while (parent[root] != root)
+            {
+                signtoroot *= sign[root];
+                root = parent[root];
+            }
+            labelsign = signtoroot;          
+            while (parent[idx] != root)
+            {
+                int p = parent[idx];
+                int s = signtoroot * sign[idx];
+                parent[idx] = root;
+                sign[idx] = signtoroot;
+                idx = p;
+                signtoroot = s;
+            }
+            label = root;              
+        }
+
+        void unionTogether(int idx1, int idx2, int labelsign)
+        {
+            int l1, ls1, l2, ls2;
+            find(idx1, l1, ls1);
+            find(idx2, l2, ls2);
+            parent[l1] = l2;
+            sign[l1] = labelsign * ls1 * ls2;
+        }
+
+    private:
+        std::vector<int> parent;
+        std::vector<int> sign;
+    };
+
 
     bool integrate(const Eigen::MatrixXd& V, const FrameField& field, Eigen::MatrixXd& soupValues, double scale, 
         double MIPtol, CubeCoverOptions::ParameterizationType type, bool forceBoundaryAlignment, bool verbose)
@@ -42,25 +93,71 @@ namespace CubeCover {
         }
 
         int ntets = mesh.nTets();
-
         int vpf = field.vectorsPerFrame();
-
-        // three parameter values per soup vertex
-        int regulardofs = vpf * 4 * ntets;
 
         soupValues.resize(4 * ntets, vpf);
         if (ntets == 0)
             return true;
 
-        // three jump variables across each face
-        int jumpdofs = vpf * cutFaces.size();
-
         std::set<int> cutfaceset;
         for (auto it : cutFaces)
             cutfaceset.insert(it);
 
+        // three parameter values per soup vertex
+        int soupdofs = 4 * ntets * vpf;
+        UnionFind uf(soupdofs);
+
+        // merge together equivalent DOFs
+        int nfaces = mesh.nFaces();
+        for (int i = 0; i < mesh.nFaces(); i++)
+        {
+            if (mesh.isBoundaryFace(i))
+                continue;
+
+            int fromtet = mesh.faceTet(i, 0);
+            int totet = mesh.faceTet(i, 1);
+            AssignmentGroup o = field.faceAssignment(i);
+            int fromidx[3];
+            int toidx[3];
+            for (int j = 0; j < 3; j++)
+            {
+                fromidx[j] = vpf * 4 * fromtet + vpf * mesh.faceTetVertexIndex(i, 0, j);
+                toidx[j] = vpf * 4 * totet + vpf * mesh.faceTetVertexIndex(i, 1, j);
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                for (int k = 0; k < vpf; k++)
+                {
+                    int destindex = o.targetVector(k);
+                    int destsign = o.targetSign(k);
+                    if (!cutfaceset.count(i))
+                    {
+                        uf.unionTogether(fromidx[j] + k, toidx[j] + destindex, destsign);
+                    }                    
+                }
+            }            
+        }
+
+        std::map<int, int> labelmap;
+        int reduceddofs = 0;
+        for (int i = 0; i < soupdofs; i++)
+        {
+            int l, s;
+            uf.find(i, l, s);
+            auto it = labelmap.find(l);
+            if (it == labelmap.end())
+            {
+                labelmap[l] = reduceddofs++;
+            }
+        }
+
+        // three jump variables across each face
+        int jumpdofs = vpf * cutFaces.size();
+
+
+
         if(verbose)
-            std::cout << "MIP problem has " << regulardofs << " real variables and " << jumpdofs << " integer variables" << std::endl;
+            std::cout << "MIP problem has " << reduceddofs + jumpdofs << " total variables and " << jumpdofs << " integer variables" << std::endl;
 
         int nconstraints = 0;
         int intdofidx = 0;
@@ -86,13 +183,18 @@ namespace CubeCover {
                 {
                     int destindex = o.targetVector(k);
                     int destsign = o.targetSign(k);
-                    Ccoeffs.push_back({ nconstraints, fromidx[j] + k, 1.0 });
-                    Ccoeffs.push_back({ nconstraints, toidx[j] + destindex, -double(destsign) });
                     if (cutfaceset.count(i))
                     {
-                        Ccoeffs.push_back({ nconstraints, regulardofs + intdofidx + k, 1.0 });
+                        int froml, froms;
+                        uf.find(fromidx[j] + k, froml, froms);
+                        Ccoeffs.push_back({ nconstraints, labelmap[froml], double(froms) });
+                        int tol, tos;
+                        uf.find(toidx[j] + destindex, tol, tos);                        
+                        Ccoeffs.push_back({ nconstraints, labelmap[tol], double(-tos * destsign) });
+                        Ccoeffs.push_back({ nconstraints, reduceddofs + intdofidx + k, 1.0 });
+                        nconstraints++;
                     }
-                    nconstraints++;
+                    
                 }
             }
             if (cutfaceset.count(i))
@@ -101,13 +203,15 @@ namespace CubeCover {
         // pin one corner
         for (int i = 0; i < vpf; i++)
         {
-            Ccoeffs.push_back({ nconstraints, i, 1.0 });
+            int l, s;
+            uf.find(i, l, s);
+            Ccoeffs.push_back({ nconstraints, labelmap[l], 1.0 });
             nconstraints++;
         }
         if(verbose)
             std::cout << "Enforcing " << nconstraints << " constraints" << std::endl;
         assert(intdofidx == jumpdofs);
-        Eigen::SparseMatrix<double> C(nconstraints, regulardofs + jumpdofs + 1);
+        Eigen::SparseMatrix<double> C(nconstraints, reduceddofs + jumpdofs + 1);
         C.setFromTriplets(Ccoeffs.begin(), Ccoeffs.end());
 
 
@@ -134,7 +238,9 @@ namespace CubeCover {
                 {
                     for (int l = 0; l < vpf; l++)
                     {
-                        Dcoeffs.push_back({ 3 * vpf * i + 3 * l + k, 12 * i + vpf * j + l, Grad(k,j) });
+                        int label, sign;
+                        uf.find(12 * i + vpf * j + l, label, sign);
+                        Dcoeffs.push_back({ 3 * vpf * i + 3 * l + k, labelmap[label], sign * Grad(k,j) });
                     }
                 }
             }
@@ -146,7 +252,7 @@ namespace CubeCover {
                 }
             }
         }
-        Eigen::SparseMatrix<double> D(3 * vpf * ntets, regulardofs + jumpdofs);
+        Eigen::SparseMatrix<double> D(3 * vpf * ntets, reduceddofs + jumpdofs);
         D.setFromTriplets(Dcoeffs.begin(), Dcoeffs.end());
 
         // inner product
@@ -182,7 +288,7 @@ namespace CubeCover {
             intdofs.resize(jumpdofs);
             for (int i = 0; i < jumpdofs; i++)
             {
-                intdofs[i] = regulardofs + i;
+                intdofs[i] = reduceddofs + i;
             }
         }
 
@@ -201,7 +307,7 @@ namespace CubeCover {
             {
                 std::cout << "Integer variables: ";
                 for (int i = 0; i < jumpdofs; i++)
-                    std::cout << result[regulardofs + i] << " ";
+                    std::cout << result[reduceddofs + i] << " ";
                 std::cout << std::endl;
             }
             if (verbose)
@@ -216,7 +322,9 @@ namespace CubeCover {
                 {
                     for (int k = 0; k < vpf; k++)
                     {
-                        soupValues(4 * i + j, k) = result[vpf * 4 * i + vpf * j + k];
+                        int label, sign;
+                        uf.find(vpf * 4 * i + vpf * j + k, label, sign);
+                        soupValues(4 * i + j, k) = result[labelmap[label]] * sign;
                     }
                 }
             }
