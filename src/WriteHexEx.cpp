f4e7e5ea2317a11dc2b615373dee7013f617856f
fool <josh@cs.utexas.edu>
2021-08-31 15:19:53 -0500

    cleanup debugging cruft

--- src/WriteHexEx.cpp
+++ src/WriteHexEx.cpp
@@ -45,45 +45,9 @@ namespace CubeCover
         for (int i = 0; i < ntets; i++)
         {
             bool hasVol = true; 
-            // for (int dir = 0; dir < 3; dir++)
-            // {            
-            //     int sample1 = (dir + 1) % 3;
-            //     int sample2 = (dir + 2) % 3;
-
-            //     for (int vert1 = 0; vert1 < 4; vert1++)
-            //     {
-            //         for (int vert2 = vert1 + 1; vert2 < 4; vert2++)
-            //         {
-            //             for (int vert3 = vert2 + 1; vert3 < 4; vert3++)
-            //             {
-            //                 double s11val = vals(4 * i + vert1, sample1);
-            //                 double s12val = vals(4 * i + vert2, sample1);
-            //                 double s13val = vals(4 * i + vert3, sample1);
-            //                 double s21val = vals(4 * i + vert1, sample2);
-            //                 double s22val = vals(4 * i + vert2, sample2);
-            //                 double s23val = vals(4 * i + vert3, sample2);
-                                    
-            //                 Eigen::Matrix2d M;
-            //                 M << s12val - s11val, s13val - s11val,
-            //                      s22val - s21val, s23val - s21val;
-
-            //                 if (M.determinant() == 0)
-            //                 {
-            //                     hasVol = false;
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
 
             if(hasVol)
             {
-                // int tmp = T(i, 0);
-                // T(i, 0) = T(i, 1);
-                // T(i, 1) = tmp;
-                // Eigen::VectorXd tmpv = vals.row(4*i);
-                // vals.row(4*i) = vals.row(4*i + 1);
-                // vals.row(4*i + 1) = tmpv;
                 for (int j = 0; j < 4; j++)
                 {
                     if (j != 0)
